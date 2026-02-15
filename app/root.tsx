5bdb97beb8d1396d53482fa20f8fd637ea11b914
Calum McDonald <calumcdonald@gmail.com>
2026-02-15 20:02:16 +0000

    Add filters and unavailable icon

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,19 @@ import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/node";
 import styles from "./styles/global.css?url";
 
-export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap",
+  },
+];
 
 export default function App() {
   return (
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,8 +2,11 @@ import { useFetcher, useLoaderData } from "@remix-run/react";
 import dayjs from "dayjs";
 import { useState } from "react";
 import { prisma } from "~/utils/db.server";
+import deleteVideo from "~/utils/deleteVideo.server";
 import getVideos from "~/utils/getVideos.server";
 import syncPlaylist from "~/utils/syncPlaylist.server";
+
+type Filter = "all" | "downloaded" | "available" | "unavailable";
 
 export async function action() {
   const metadataKey = "last_refresh";
@@ -37,7 +40,31 @@ const Playlist = () => {
   const data = useLoaderData<typeof loader>();
   const fetcher = useFetcher();
 
+  const [filter, setFilter] = useState<Filter>("all");
   const [lastResync, setLastResync] = useState(data.lastResync);
+
+  const filters: { label: string; type: Filter }[] = [
+    {
+      label: `${data.videos.length} total`,
+      type: "all",
+    },
+    {
+      label: `${data.videos.filter((v) => v.filename).length} downloaded`,
+      type: "downloaded",
+    },
+    {
+      label: `${
+        data.videos.filter((v) => v.status === "AVAILABLE").length
+      } available`,
+      type: "available",
+    },
+    {
+      label: `${
+        data.videos.filter((v) => v.status === "UNAVAILABLE").length
+      } unavailable`,
+      type: "unavailable",
+    },
+  ];
 
   const minuteDifference = Math.round(dayjs().diff(lastResync) / 1000 / 60);
 
@@ -47,15 +74,42 @@ const Playlist = () => {
     fetcher.submit({}, { method: "POST" });
   };
 
+  const filterVideos = (videos: typeof data.videos) => {
+    if (filter === "available")
+      return videos.filter((video) => video.status === "AVAILABLE");
+    if (filter === "unavailable")
+      return videos.filter((video) => video.status === "UNAVAILABLE");
+    if (filter === "downloaded")
+      return videos.filter((video) => video.filename);
+
+    return videos;
+  };
+
+  const handleFilterClick = (type: Filter) => {
+    setFilter(type);
+  };
+
   return (
     <div>
-      <button onClick={handleResyncClick}>
-        Re-Sync{" "}
-        {lastResync && minuteDifference > 1 && `(${minuteDifference}m ago)`}
-      </button>
+      <div className="control-panel">
+        <button onClick={handleResyncClick}>
+          Re-Sync{" "}
+          {lastResync && minuteDifference > 1 && `(${minuteDifference}m ago)`}
+        </button>
+        {filters.map((filterItem) => (
+          <div
+            className={`filter ${
+              filter === filterItem.type ? "selected" : undefined
+            }`}
+            onClick={() => handleFilterClick(filterItem.type)}
+          >
+            {filterItem.label}
+          </div>
+        ))}
+      </div>
 
       <div className="playlist-container">
-        {data.videos.map((video) => (
+        {filterVideos(data.videos).map((video) => (
           <div className="video-card">
             <a
               href={video.filename ? `/videos/${video.filename}` : undefined}
@@ -70,13 +124,16 @@ const Playlist = () => {
             </div> */}
               <img
                 className={`video-thumb${
-                  video.status === "UNAVAILABLE" || !video.filename
-                    ? " unavailable"
-                    : ""
+                  !video.filename ? " unavailable" : ""
                 }${!video.thumbnailUrl ? " placeholder" : ""}`}
                 src={video.thumbnailUrl ?? "/placeholder.webp"}
               />
             </a>
+            {video.status === "UNAVAILABLE" ? (
+              <img className="unavailable-icon" src="/yt-unavailable.svg" />
+            ) : (
+              <></>
+            )}
           </div>
         ))}
       </div>
