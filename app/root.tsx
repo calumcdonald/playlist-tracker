7ea14557bbc14afa69ae5eedcd910cf2a82af550
Calum McDonald <calumcdonald@gmail.com>
2026-02-15 00:21:58 +0000

    styling and title

--- app/root.tsx
+++ app/root.tsx
@@ -1,9 +1,14 @@
 import { Links, Meta, Outlet, Scripts } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import styles from "./styles/global.css?url";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export default function App() {
   return (
     <html>
       <head>
+        <title>Why? Exactly</title>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
         <Meta />
         <Links />
